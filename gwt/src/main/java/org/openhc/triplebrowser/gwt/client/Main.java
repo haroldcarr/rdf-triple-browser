fddcc074dc8a14cc6b2dd3aece8a0f0ff7b89ece
Harold Carr <harold.carr@gmail.com>
2006-06-15 03:30:13 +0000

    ["Check in before change.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/client/Main.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/client/Main.java
@@ -1,7 +1,13 @@
+//
+// Created       : 2006 Jun 14 (Wed) 18:29:38 by Harold Carr.
+// Last Modified : 2006 Jun 14 (Wed) 20:29:57 by Harold Carr.
+//
+
 package com.differentity.client;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Button;
@@ -35,6 +41,9 @@ public class Main
     private static String expand   = "expand";
     private static String differentityDotCom = "differentity.com";
     private static String copyright = "copyright 2006";
+    private static String subject = "subject";
+    private static String verb = "verb";
+    private static String object = "object";
     private static String subjectVerbObject = "subjectVerbObject";
 
     /**
@@ -42,24 +51,18 @@ public class Main
      */
     public void onModuleLoad() {
 
-	Button currentButton = null;
-	HTML   currentHTML   = null;
-
 	Widget[] result = null;
-	result = buildSVOPanel();
+	result = buildSVOPanel(new SVOManager(subject));
 	VerticalPanel subjectVerticalPanel = (VerticalPanel) result[0];
 	Button subjectExpandCollapseButton = (Button) result[1];
-	HTML subjectHTML                   = (HTML) result[2];
 
-	result = buildSVOPanel();
+	result = buildSVOPanel(new SVOManager(verb));
 	VerticalPanel verbVerticalPanel    = (VerticalPanel) result[0];
 	Button verbExpandCollapseButton    = (Button) result[1];
-	HTML verbHTML                      = (HTML) result[2];
 
-	result = buildSVOPanel();
+	result = buildSVOPanel(new SVOManager(object));
 	VerticalPanel objectVerticalPanel  = (VerticalPanel) result[0];
 	Button objectExpandCollapseButton  = (Button) result[1];
-	HTML objectHTML                    = (HTML) result[2];
 
 	HorizontalPanel horizontalPanel = new HorizontalPanel();
 	horizontalPanel.setVerticalAlignment(VerticalPanel.ALIGN_MIDDLE);
@@ -84,83 +87,129 @@ public class Main
 	RootPanel.get("slot1").add(dockPanel);
     }
 
-    private Widget[] buildSVOPanel()
+    private Widget[] buildSVOPanel(final SVOManager svoManager)
     {
 	VerticalPanel verticalPanel = new VerticalPanel();
 	verticalPanel.setHorizontalAlignment(VerticalPanel.ALIGN_LEFT);
-	Button button = new Button(collapse);
+	Button button = new Button(expand); // STATE
 	button.addClickListener(new ClickListener() {
 	    public void onClick(Widget sender) {
 		Button x = (Button) sender;
 		if (x.getText().equals(collapse)) {
+		    svoManager.collapse();
 		    x.setText(expand);
 		} else {
+		    svoManager.expand();
 		    x.setText(collapse);
 		}
 	    }
 	});
 	verticalPanel.add(button);
-	HTML html = buildSVO();
-	ScrollPanel scrollPanel = new ScrollPanel(html);
+	ScrollPanel scrollPanel = new ScrollPanel(svoManager.getHTML());
 	scrollPanel.setStyleName(subjectVerbObject);
 	verticalPanel.add(scrollPanel);
-	return new Widget[] { verticalPanel,  button, html };
+	return new Widget[] { verticalPanel,  button };
+    }
+}
+
+class SVOManager
+{
+    String name;
+    List contents;
+    HTML html;
+
+    SVOManager(String name)
+    {
+	this.name = name;
+	this.contents = svoList;
+	this.html = new HTML();
+	collapse();
     }
 
-    private HTML buildSVO()
+    HTML getHTML()
     {
-	Iterator i = svoList.iterator();
+	return html;
+    }
+
+    void expand()
+    {
+	expandCollapse(true);
+    }
+
+    void collapse()
+    {
+	expandCollapse(false);
+    }
+
+    private void expandCollapse(boolean isExpand)
+    {
+	Iterator i = contents.iterator();
 	StringBuffer result = new StringBuffer();
 	while (i.hasNext()) {
-	    result.append(i.next());
+	    String item = (String) i.next();
+	    if (isExpand) {
+		result.append(item);
+	    } else {
+		result.append(lastSlash(item));
+	    }
 	    result.append("<br/>");
 	}
-	return new HTML(result.toString());
+	html.setHTML(result.toString());
     }
 
-    ArrayList svoList = new ArrayList();
-    { // NOTE: block needed for this to compile. 
-      // And x to get result needed too.
-    boolean x = true;
-    x = svoList.add("http://differentity.com/haroldcarr/author");
-    x = svoList.add("http://differentity.com/haroldcarr/authorPrefix");
-    x = svoList.add("http://differentity.com/haroldcarr/authorFirstName");
-    x = svoList.add("http://differentity.com/haroldcarr/authorMiddleName");
-    x = svoList.add("http://differentity.com/haroldcarr/authorLastName");
-    x = svoList.add("http://differentity.com/haroldcarr/authorSuffix");
-    x = svoList.add("http://differentity.com/haroldcarr/authorBorn");
-    x = svoList.add("http://differentity.com/haroldcarr/authorDied");
-    x = svoList.add("http://differentity.com/haroldcarr/authorIdea");
-    x = svoList.add("http://differentity.com/haroldcarr/author");
-    x = svoList.add("http://differentity.com/haroldcarr/ideaInCategory");
-    x = svoList.add("http://differentity.com/haroldcarr/work");
-    x = svoList.add("http://differentity.com/haroldcarr/workAuthor");
-    x = svoList.add("http://differentity.com/haroldcarr/workTitle");
-    x = svoList.add("http://differentity.com/haroldcarr/workPublished");
-    x = svoList.add("http://differentity.com/haroldcarr/workWritten");
-    x = svoList.add("http://differentity.com/haroldcarr/bataille");
-    x = svoList.add("http://differentity.com/haroldcarr/book");
-    x = svoList.add("http://differentity.com/haroldcarr/guilty");
-    x = svoList.add("http://differentity.com/haroldcarr/eroticism");
-    x = svoList.add("http://differentity.com/haroldcarr/blue_of_noon");
-    x = svoList.add("http://differentity.com/haroldcarr/inner_experience");
+    private String lastSlash(String x)
+    {
+	int lastSlash = 0;
+	int i;
+	for (i = 0; i < x.length(); ++i) {
+	    if (x.charAt(i) == '/') {
+		lastSlash = i;
+	    }
+	}
+	return x.substring(lastSlash + 1);
+    }
 
-    x = svoList.add("http://differentity.com/haroldcarr/similarTo");
-    x = svoList.add("http://differentity.com/haroldcarr/equalTo");
-    x = svoList.add("http://differentity.com/haroldcarr/contraryTo");
-    x = svoList.add("http://differentity.com/haroldcarr/contrarstWith");
-    x = svoList.add("http://differentity.com/haroldcarr/relatesTo");
-    x = svoList.add("http://differentity.com/haroldcarr/fate");
-    x = svoList.add("http://differentity.com/haroldcarr/formlessness");
-    x = svoList.add("http://differentity.com/haroldcarr/god");
-    x = svoList.add("http://differentity.com/haroldcarr/stability");
-    x = svoList.add("http://differentity.com/haroldcarr/reason");
-    x = svoList.add("http://differentity.com/haroldcarr/nothingness");
-    x = svoList.add("http://differentity.com/haroldcarr/chance");
-    x = svoList.add("http://differentity.com/haroldcarr/strength");
-    x = svoList.add("http://differentity.com/haroldcarr/death");
-    x = svoList.add("http://differentity.com/haroldcarr/anguish");
-    x = svoList.add("http://differentity.com/haroldcarr/silence");
-    x = svoList.add("http://differentity.com/haroldcarr/limit");
+    private List svoList = new ArrayList();
+    { // NOTE: block needed for this to compile. 
+    svoList.add("http://differentity.com/haroldcarr/author");
+    svoList.add("http://differentity.com/haroldcarr/authorPrefix");
+    svoList.add("http://differentity.com/haroldcarr/authorFirstName");
+    svoList.add("http://differentity.com/haroldcarr/authorMiddleName");
+    svoList.add("http://differentity.com/haroldcarr/authorLastName");
+    svoList.add("http://differentity.com/haroldcarr/authorSuffix");
+    svoList.add("http://differentity.com/haroldcarr/authorBorn");
+    svoList.add("http://differentity.com/haroldcarr/authorDied");
+    svoList.add("http://differentity.com/haroldcarr/authorIdea");
+    svoList.add("http://differentity.com/haroldcarr/author");
+    svoList.add("http://differentity.com/haroldcarr/ideaInCategory");
+    svoList.add("http://differentity.com/haroldcarr/work");
+    svoList.add("http://differentity.com/haroldcarr/workAuthor");
+    svoList.add("http://differentity.com/haroldcarr/workTitle");
+    svoList.add("http://differentity.com/haroldcarr/workPublished");
+    svoList.add("http://differentity.com/haroldcarr/workWritten");
+    svoList.add("http://differentity.com/haroldcarr/bataille");
+    svoList.add("http://differentity.com/haroldcarr/book");
+    svoList.add("http://differentity.com/haroldcarr/guilty");
+    svoList.add("http://differentity.com/haroldcarr/eroticism");
+    svoList.add("http://differentity.com/haroldcarr/blue_of_noon");
+    svoList.add("http://differentity.com/haroldcarr/inner_experience");
+
+    svoList.add("http://differentity.com/haroldcarr/similarTo");
+    svoList.add("http://differentity.com/haroldcarr/equalTo");
+    svoList.add("http://differentity.com/haroldcarr/contraryTo");
+    svoList.add("http://differentity.com/haroldcarr/contrarstWith");
+    svoList.add("http://differentity.com/haroldcarr/relatesTo");
+    svoList.add("http://differentity.com/haroldcarr/fate");
+    svoList.add("http://differentity.com/haroldcarr/formlessness");
+    svoList.add("http://differentity.com/haroldcarr/god");
+    svoList.add("http://differentity.com/haroldcarr/stability");
+    svoList.add("http://differentity.com/haroldcarr/reason");
+    svoList.add("http://differentity.com/haroldcarr/nothingness");
+    svoList.add("http://differentity.com/haroldcarr/chance");
+    svoList.add("http://differentity.com/haroldcarr/strength");
+    svoList.add("http://differentity.com/haroldcarr/death");
+    svoList.add("http://differentity.com/haroldcarr/anguish");
+    svoList.add("http://differentity.com/haroldcarr/silence");
+    svoList.add("http://differentity.com/haroldcarr/limit");
     }
 }
