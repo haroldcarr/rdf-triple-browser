c7c71e5a07379103381f514ea110ad8462a54acd
Harold Carr <harold.carr@gmail.com>
2006-08-14 03:41:35 +0000

    ["Move all knowledge of Jena formatting into this file.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/server/Jena.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/server/Jena.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Jul 28 (Fri) 14:21:09 by Harold Carr.
-// Last Modified : 2006 Jul 29 (Sat) 09:20:00 by Harold Carr.
+// Last Modified : 2006 Aug 12 (Sat) 22:24:34 by Harold Carr.
 //
 
 package com.differentity.server;
@@ -14,10 +14,13 @@ import com.hp.hpl.jena.rdql.Query;
 import com.hp.hpl.jena.rdql.QueryEngine;
 import com.hp.hpl.jena.rdql.QueryResults;
 
+import com.differentity.client.Main;
+
 public class Jena
 {
     public final String NOWHERE = "http://nowhere/";
     public final String RDF_XML = "RDF/XML";
+    public static String lastQueryString; // for debug
 
     Model model;
 
@@ -46,12 +49,60 @@ public class Jena
 	}
     }
 
+    public QueryResults doQuery(String s, String v, String o)
+    {
+	String subject = formatInput(s);
+	String verb    = formatInput(v);
+	String object  = formatInput(o);
+	String selectVars = "";
+	if (subject.startsWith(Main.questionMarkSymbol)) {
+	    selectVars = selectVars + " " + subject;
+	}
+	if (verb.startsWith(Main.questionMarkSymbol)) {
+	    selectVars = selectVars + " " + verb;
+	}
+	if (object.startsWith(Main.questionMarkSymbol)) {
+	    selectVars = selectVars + " " + object;
+	}
+
+	String queryString =
+	    " SELECT " + selectVars +
+	    " WHERE ( " +
+	    subject + " " +
+	    verb    + " " +
+	    object  + " )";
+
+	// Spent a lot of time here trying to figure out why URIs
+	// disappeared in the status output.
+	// It's simple: because they look like "<URI>" - when
+	// that is put in an HTML element it disappears as an unknown tag.
+	// TODO: make it visible - either "ampersand" it our change
+	// status area from HTML to TextBox.
+
+	lastQueryString =
+	    " Jena: " +
+	    "[subject " + s + " / " + subject + "]" +
+	    "[verb "    + v + " / " + verb    + "]" +
+	    "[object "  + o + " / " + object  + "]" + "<br/>" +
+	    queryString;
+
+	return doQuery(queryString);
+    }
+
     public QueryResults doQuery(String queryString)
     {
-	Query query =new Query(queryString);
+	Query query = new Query(queryString);
 	query.setSource(model);
 	QueryEngine qe = new QueryEngine(query);
 	return qe.exec();
+    }
+
+    private String formatInput(String x)
+    {
+	if (x.startsWith("?")) {
+	    return x;
+	}
+	return "<" + x + ">";
     }
 }
 
