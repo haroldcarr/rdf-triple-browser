ca4ccf003788f9cc514eab9442bc93c9cd64c68f
Harold Carr <harold.carr@gmail.com>
2006-08-14 03:42:26 +0000

    ["First real response from Jena.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/server/ServiceImpl.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/server/ServiceImpl.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Jul 28 (Fri) 17:52:12 by Harold Carr.
-// Last Modified : 2006 Aug 12 (Sat) 19:19:39 by Harold Carr.
+// Last Modified : 2006 Aug 13 (Sun) 20:31:42 by Harold Carr.
 //
 
 package com.differentity.server;
@@ -9,15 +9,20 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
-import com.differentity.server.Jena;
+import com.hp.hpl.jena.rdql.QueryResults;
+import com.hp.hpl.jena.rdql.ResultBinding;
 
+import com.differentity.client.Main;
 import com.differentity.client.QueryRequest;
 import com.differentity.client.QueryResponse;
 import com.differentity.client.Service;
+
+import com.differentity.server.Jena;
 
 public class ServiceImpl
     extends RemoteServiceServlet
@@ -47,7 +52,104 @@ public class ServiceImpl
 	return "initialization complete";
     }
 
+    private boolean firstTime = false;
+
     public QueryResponse doQuery(QueryRequest queryRequest)
+    {
+	if (firstTime) {
+	    firstTime = false;
+	    return doQueryTest(queryRequest);
+	}
+	QueryResults queryResults = 
+	    jena.doQuery(queryRequest.getSubject(),
+			 queryRequest.getVerb(),
+			 queryRequest.getObject());
+
+	return makeResponse(queryRequest, queryResults);
+    }
+
+    private QueryResponse makeResponse(QueryRequest queryRequest,
+				       QueryResults queryResults)
+    {
+	String status =
+	    " Service: " +
+	    "[subject " + queryRequest.getSubject() + "]" +
+	    "[verb "    + queryRequest.getVerb()    + "]" +
+	    "[object "  + queryRequest.getObject()  + "]" + "<br/>" +
+	    Jena.lastQueryString;
+	/*
+	List first  = new ArrayList();
+	List second = new ArrayList();
+	List third  = new ArrayList();
+	first.add(queryRequest.getSubject());
+	second.add(queryRequest.getVerb());
+	third.add(queryRequest.getObject());
+
+	List list = queryResults.getResultVars();
+	Iterator i = list.iterator();
+	if (i.hasNext()) {
+	    first.add(0, "http://one/" + i.next());
+	}
+	if (i.hasNext()) {
+	    second.add(0, "http://two/" + i.next());
+	}
+	if (i.hasNext()) {
+	    third.add(0, "http://three/" + i.next());
+	}
+	return new QueryResponse(first, second, third, status);
+	*/
+	boolean isSubjectVar = false;
+	boolean isVerbVar = false;
+	boolean isObjectVar = false;
+	List list = queryResults.getResultVars();
+	Iterator i = list.iterator();
+	while (i.hasNext()) {
+	    String varName = (String) i.next();
+	    if (varName.equals(Main.subject)) {
+		isSubjectVar = true;
+	    } else if (varName.equals(Main.verb)) {
+		isVerbVar = true;
+	    } else if (varName.equals(Main.object)) {
+		isObjectVar = true;
+	    }
+	}
+	List subjectResponse = new ArrayList();
+	List verbResponse    = new ArrayList();
+	List objectResponse  = new ArrayList();
+	if (!isSubjectVar) { subjectResponse.add(queryRequest.getSubject()); }
+	if (!isVerbVar)    { verbResponse.add   (queryRequest.getVerb()); }
+	if (!isObjectVar)  { objectResponse.add (queryRequest.getObject()); }
+	while (queryResults.hasNext()) {
+	    ResultBinding resultBinding = (ResultBinding) queryResults.next();
+	    if (isSubjectVar) { 
+		String x = resultBinding.get(Main.subject).toString();
+		if (! subjectResponse.contains(x)) {
+		    subjectResponse.add(x);
+		}
+	    }
+	    if (isVerbVar) {
+		String x = resultBinding.get(Main.verb).toString();
+		if (! verbResponse.contains(x)) {
+		    verbResponse.add(x);
+		}
+	    }
+	    if (isObjectVar) {
+		String x = resultBinding.get(Main.object).toString();
+		if (! objectResponse.contains(x)) {
+		    objectResponse.add(x);
+		}
+	    }
+	}
+	return new QueryResponse(subjectResponse, verbResponse, objectResponse,
+				 status);
+    }
+
+    ////////////////////////////////////////////////////
+    //
+    // Test.
+    //
+
+    private QueryResponse doQueryTest(QueryRequest queryRequest)
     {
 	List subjectList = (List) svoList.clone();
 	List verbList    = (List) svoList.clone();
@@ -60,7 +162,7 @@ public class ServiceImpl
 	objectList.add(0, "http://object.com/");
 	objectList.add(0, queryRequest.getObject());
 
-	return new QueryResponse(subjectList, verbList, objectList);
+	return new QueryResponse(subjectList, verbList, objectList, "TEST");
     }
 
     private ArrayList svoList = new ArrayList();
