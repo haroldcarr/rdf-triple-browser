09ced34344358b1b9bc0a0f6729dfce9d481de6a
Harold Carr <harold.carr@gmail.com>
2006-09-13 01:49:21 +0000

    ["Experimenting with menus.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/client/Test.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/client/Test.java
@@ -1,12 +1,13 @@
 //
 // Created       : 2006 Jul 30 (Sun) 15:53:20 by Harold Carr.
-// Last Modified : 2006 Jul 30 (Sun) 15:59:40 by Harold Carr.
+// Last Modified : 2006 Sep 10 (Sun) 20:02:37 by Harold Carr.
 //
 
 package com.differentity.client;
 
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.MenuBar;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Test
@@ -15,14 +16,23 @@ public class Test
 
     public Test()
     {
-	FlexTable t = new FlexTable();
-	t.setText(0, 0, "upper-left corner");
-	t.setText(2, 2, "bottom-right corner");
-	
-	t.setWidget(1, 0, new Button("Wide Button"));
-	t.getFlexCellFormatter().setColSpan(1, 0, 3);
 
-	widget = t;
+	Command cmd = new Command() {
+	    public void execute() {
+		Window.alert("You selected a menu item!");
+	    }
+	};
+
+	MenuBar fooMenu = new MenuBar(true);
+	fooMenu.addItem("*", cmd);
+	fooMenu.addItem("->", cmd);
+	fooMenu.addItem("<-", cmd);
+	fooMenu.addItem("new", cmd);
+	fooMenu.addItem("all", cmd);
+	MenuBar barMenu = new MenuBar();
+	barMenu.addItem("xxx", fooMenu);
+	barMenu.setAutoOpen(true);
+	widget = barMenu;
     }
 
     public Widget getWidget() { return widget; }
