179b552e68702233d5d6617eab6c4489c67cf179
Harold Carr <harold.carr@gmail.com>
2006-09-13 01:47:30 +0000

    ["Added \"setContentsOf\" that is passed to server then passed right back to\n", "client as part of response.  Used to limit which triple column is filled in.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/common/QueryRequest.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/common/QueryRequest.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Aug 12 (Sat) 14:56:41 by Harold Carr.
-// Last Modified : 2006 Aug 20 (Sun) 13:27:45 by Harold Carr.
+// Last Modified : 2006 Sep 10 (Sun) 20:50:51 by Harold Carr.
 //
 
 package com.differentity.client;
@@ -15,20 +15,24 @@ public class QueryRequest
     private String subject;
     private String property;
     private String value;
+    private String setContentsOf;
 
     // GWT requires zero-arg constructor.
     public QueryRequest() {}
 
-    public QueryRequest(String subject, String property, String value)
+    public QueryRequest(final String subject, final String property, 
+			final String value, final String setContentsOf)
     {
-	this.subject  = subject;
-	this.property = property;
-	this.value    = value;
+	this.subject       = subject;
+	this.property      = property;
+	this.value         = value;
+	this.setContentsOf = setContentsOf;
     }
 
-    public String getSubject()  { return subject; }
-    public String getProperty() { return property; }
-    public String getValue()    { return value; }
+    public String getSubject()       { return subject; }
+    public String getProperty()      { return property; }
+    public String getValue()         { return value; }
+    public String getSetContentsOf() { return setContentsOf; }
 }
 
 // End of file.
--- gwt/src/main/java/org/openhc/triplebrowser/gwt/common/QueryResponse.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/common/QueryResponse.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Aug 12 (Sat) 14:56:41 by Harold Carr.
-// Last Modified : 2006 Aug 20 (Sun) 13:28:06 by Harold Carr.
+// Last Modified : 2006 Sep 10 (Sun) 20:48:10 by Harold Carr.
 //
 
 package com.differentity.client;
@@ -30,18 +30,22 @@ public class QueryResponse
      */
     private List value;
 
+    private String setContentsOf;
+
     private String status;
 
     // GWT requires zero arg constructor.
     public QueryResponse() {}
 
     public QueryResponse(final List subject, final List property, 
-			 final List value, final String status)
+			 final List value, final String setContentsOf,
+			 final String status)
     {
-	this.subject  = subject;
-	this.property = property;
-	this.value    = value;
-	this.status   = status;
+	this.subject       = subject;
+	this.property      = property;
+	this.value         = value;
+	this.setContentsOf = setContentsOf;
+	this.status        = status;
     }
 
     /**
@@ -58,6 +62,8 @@ public class QueryResponse
      * @gwt.typeArgs <java.lang.String>
      */
     public List getValue() { return value; }
+
+    public String getSetContentsOf() { return setContentsOf; }
 
     public String getStatus() { return status; }
 }
