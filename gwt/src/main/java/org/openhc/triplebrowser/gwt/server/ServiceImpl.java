72b53af0cdf6abb9abe245bfd7f729bccd87ad9d
Harold Carr <harold.carr@gmail.com>
2006-10-07 23:29:22 +0000

    ["Removed development/test stuff.\n", "Use multiple value return from Jena class.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/server/ServiceImpl.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/server/ServiceImpl.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Jul 28 (Fri) 17:52:12 by Harold Carr.
-// Last Modified : 2006 Oct 06 (Fri) 21:27:12 by Harold Carr.
+// Last Modified : 2006 Oct 07 (Sat) 15:57:52 by Harold Carr.
 //
 
 package com.differentity.server;
@@ -37,7 +37,7 @@ public class ServiceImpl
     public String initialize() 
     {
 	if (initialized) {
-	    return "already initialized" + " " + params();
+	    return "already initialized";
 	}
 	
 	jena = new Jena();
@@ -53,7 +53,7 @@ public class ServiceImpl
 	    return stream.toString();
 	}
 
-	return "initialization complete" + " " + params();
+	return "initialization complete";
     }
 
     public String close() 
@@ -78,68 +78,25 @@ public class ServiceImpl
 	return "close complete";
     }
 
-    private String params()
+    public QueryResponse doQuery(final QueryRequest queryRequest)
     {
-	String result = "Method: " + getThreadLocalRequest().getMethod();
-	result += " URI: " + getThreadLocalRequest().getRequestURI();
-	result += " Path: " + getThreadLocalRequest().getServletPath();
-	result += " Parameters: ";
-	java.util.Enumeration i = getThreadLocalRequest().getParameterNames();
-	for(; i.hasMoreElements() ;) {
-	    String name = (String)i.nextElement();
-	    result += " " + name;
-	    String value = getThreadLocalRequest().getParameter(name);
-	    result += "=" + value;
-	}
-	return result;
-    }
-
-    private boolean firstTime = false;
-
-    public QueryResponse doQuery(QueryRequest queryRequest)
-    {
-	if (firstTime) {
-	    firstTime = false;
-	    return doQueryTest(queryRequest);
-	}
-	ResultSet resultSet = 
+	Jena.QueryReturnValue returnValue = 
 	    jena.doQuery(queryRequest.getSubject(),
 			 queryRequest.getProperty(),
 			 queryRequest.getValue());
 
-	return makeResponse(queryRequest, resultSet);
+	return makeResponse(queryRequest, returnValue);
     }
 
-    private QueryResponse makeResponse(QueryRequest queryRequest,
-				       ResultSet resultSet)
+    private QueryResponse makeResponse(final QueryRequest queryRequest,
+				       final Jena.QueryReturnValue returnValue)
     {
-	String status =
-	    " Service: " +
-	    "[subject "  + queryRequest.getSubject()  + "]" +
-	    "[property " + queryRequest.getProperty() + "]" +
-	    "[value "    + queryRequest.getValue()    + "]" + "<br/>" +
-	    Jena.lastQueryString;
-	/*
-	List first  = new ArrayList();
-	List second = new ArrayList();
-	List third  = new ArrayList();
-	first.add(queryRequest.getSubject());
-	second.add(queryRequest.getProperty());
-	third.add(queryRequest.getValue());
+	ResultSet resultSet = returnValue.getResultSet();
 
-	List list = resultSet.getResultVars();
-	Iterator i = list.iterator();
-	if (i.hasNext()) {
-	    first.add(0, "http://one/" + i.next());
-	}
-	if (i.hasNext()) {
-	    second.add(0, "http://two/" + i.next());
-	}
-	if (i.hasNext()) {
-	    third.add(0, "http://three/" + i.next());
-	}
-	return new QueryResponse(first, second, third, status);
-	*/
+	//
+	// Determine which parts of query were variables.
+	//
+
 	boolean isSubjectVar  = false;
 	boolean isPropertyVar = false;
 	boolean isValueVar    = false;
@@ -155,9 +112,16 @@ public class ServiceImpl
 		isValueVar    = true;
 	    }
 	}
+
 	List subjectResponse  = new ArrayList();
 	List propertyResponse = new ArrayList();
 	List valueResponse    = new ArrayList();
+
+	//
+	// For the parts of the query that are NOT variables
+	// return what was given in the query.
+	//
+
 	if (!isSubjectVar) {
 	    subjectResponse.add(queryRequest.getSubject()); 
 	}
@@ -167,10 +131,14 @@ public class ServiceImpl
 	if (!isValueVar) {
 	    valueResponse.add (queryRequest.getValue()); 
 	}
-	System.out.println(resultSet.hasNext());
+
+	//
+	// For the parts of the query that ARE variables
+	// return what was found.
+	// 
+
 	while (resultSet.hasNext()) {
 	    QuerySolution querySolution = resultSet.nextSolution();
-	    System.out.println(querySolution.toString());
 	    if (isSubjectVar) { 
 		String x = querySolution.get(Main.subject).toString();
 		if (! subjectResponse.contains(x)) {
@@ -190,9 +158,16 @@ public class ServiceImpl
 		}
 	    }
 	}
-	return new QueryResponse(subjectResponse, propertyResponse,
-				 valueResponse,
-				 queryRequest.getSetContentsOf(), status);
+	QueryResponse queryResponse = 
+	    new QueryResponse(subjectResponse, propertyResponse,
+			      valueResponse,
+			      queryRequest.getSetContentsOf(),
+			      returnValue.getQueryString());
+
+	// VERY IMPORTANT: close Jena's query engine to release resources.
+	returnValue.getQueryExecution().close();
+
+	return queryResponse;
     }
 
     public QueryResponse assertFact(QueryRequest queryRequest)
@@ -210,78 +185,6 @@ public class ServiceImpl
 	return new QueryResponse(subjectResponse, propertyResponse,
 				 valueResponse,
 				 queryRequest.getSetContentsOf(), "OK");
-    }
-
-    ////////////////////////////////////////////////////
-    //
-    // Test.
-    //
-
-    private QueryResponse doQueryTest(QueryRequest queryRequest)
-    {
-	List subjectList  = (List) spvList.clone();
-	List propertyList = (List) spvList.clone();
-	List valueList    = (List) spvList.clone();
-
-	subjectList.add(0, "http://subject.com/");
-	subjectList.add(0, queryRequest.getSubject());
-	propertyList.add(0, "http://property.com/");
-	propertyList.add(0, queryRequest.getProperty());
-	valueList.add(0, "http://value.com/");
-	valueList.add(0, queryRequest.getValue());
-
-	return new QueryResponse(subjectList, propertyList, valueList, 
-				 queryRequest.getSetContentsOf(),
-				 "TEST");
-    }
-
-    // TEST DATA
-    private ArrayList spvList = new ArrayList();
-    {
-    spvList.add("http://haroldcarr.com");
-    spvList.add("http://www.rojo.com/");
-    spvList.add("http://google.com");
-    spvList.add("http://del.icio.us/");
-    spvList.add("http://differentity.com/haroldcarr/author");
-    spvList.add("http://differentity.com/haroldcarr/authorPrefix");
-    spvList.add("http://differentity.com/haroldcarr/authorFirstName");
-    spvList.add("http://differentity.com/haroldcarr/authorMiddleName");
-    spvList.add("http://differentity.com/haroldcarr/authorLastName");
-    spvList.add("http://differentity.com/haroldcarr/authorSuffix");
-    spvList.add("http://differentity.com/haroldcarr/authorBorn");
-    spvList.add("http://differentity.com/haroldcarr/authorDied");
-    spvList.add("http://differentity.com/haroldcarr/authorIdea");
-    spvList.add("http://differentity.com/haroldcarr/author");
-    spvList.add("http://differentity.com/haroldcarr/ideaInCategory");
-    spvList.add("http://differentity.com/haroldcarr/work");
-    spvList.add("http://differentity.com/haroldcarr/workAuthor");
-    spvList.add("http://differentity.com/haroldcarr/workTitle");
-    spvList.add("http://differentity.com/haroldcarr/workPublished");
-    spvList.add("http://differentity.com/haroldcarr/workWritten");
-    spvList.add("http://differentity.com/haroldcarr/bataille");
-    spvList.add("http://differentity.com/haroldcarr/book");
-    spvList.add("http://differentity.com/haroldcarr/guilty");
-    spvList.add("http://differentity.com/haroldcarr/eroticism");
-    spvList.add("http://differentity.com/haroldcarr/blue_of_noon");
-    spvList.add("http://differentity.com/haroldcarr/inner_experience");
-
-    spvList.add("http://differentity.com/haroldcarr/similarTo");
-    spvList.add("http://differentity.com/haroldcarr/equalTo");
-    spvList.add("http://differentity.com/haroldcarr/contraryTo");
-    spvList.add("http://differentity.com/haroldcarr/contrarstWith");
-    spvList.add("http://differentity.com/haroldcarr/relatesTo");
-    spvList.add("http://differentity.com/haroldcarr/fate");
-    spvList.add("http://differentity.com/haroldcarr/formlessness");
-    spvList.add("http://differentity.com/haroldcarr/god");
-    spvList.add("http://differentity.com/haroldcarr/stability");
-    spvList.add("http://differentity.com/haroldcarr/reason");
-    spvList.add("http://differentity.com/haroldcarr/nothingness");
-    spvList.add("http://differentity.com/haroldcarr/chance");
-    spvList.add("http://differentity.com/haroldcarr/strength");
-    spvList.add("http://differentity.com/haroldcarr/death");
-    spvList.add("http://differentity.com/haroldcarr/anguish");
-    spvList.add("http://differentity.com/haroldcarr/silence");
-    spvList.add("http://differentity.com/haroldcarr/limit");
     }
 }
 
