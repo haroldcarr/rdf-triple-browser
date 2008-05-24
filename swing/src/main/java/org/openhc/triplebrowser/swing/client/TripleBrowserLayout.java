9248b24d74a65c076aaea58a3dbe82cbdb7d801a
Harold Carr <harold.carr@gmail.com>
2008-05-24 17:09:08 +0000

    ["xxx2 works - about to change xxx1 and xxx3.\n", ""]

--- swing/src/main/java/org/openhc/triplebrowser/swing/client/TripleBrowserLayout.java
+++ swing/src/main/java/org/openhc/triplebrowser/swing/client/TripleBrowserLayout.java
@@ -327,13 +327,15 @@ public class SwingView extends FrameView {
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
 	    .add(jPanel3Layout.createSequentialGroup()
-		 .add(jTextField4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 601, Short.MAX_VALUE)
-		 .add(x2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 601, Short.MAX_VALUE)
-	     )
-        );
+		 .add(jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+		      .add(jTextField4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 601, Short.MAX_VALUE)
+		      .add(x2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 601, Short.MAX_VALUE)
+		      )
+		 )
+	    );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(jPanel3Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+            .add(jPanel3Layout.createSequentialGroup()
 		 .add(jTextField4, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE)
 		 .add(x2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 25, Short.MAX_VALUE)
 	    )
