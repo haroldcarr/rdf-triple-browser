23ccde4e9d1b4b9e2d45d191ad284a78e237fc49
Harold Carr <harold.carr@gmail.com>
2008-05-27 17:41:44 +0000

    ["Initial revision\n", ""]

---
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/common/Util.java
@@ -0,0 +1,30 @@
+//
+// Created       : 2006 Aug 12 (Sat) 14:56:41 by Harold Carr.
+// Last Modified : 2008 May 27 (Tue) 10:36:52 by Harold Carr.
+//
+
+package org.openhc.trowser.gwt.common;
+
+public abstract class Util
+{
+    public static String substringAfterLastSlashOrFirstSharp(final String x)
+    {
+	int indexOfLastSlashOrFirstSharp = 0;
+	int i;
+	for (i = 0; i < x.length(); ++i) {
+	    if (x.charAt(i) == '/' || x.charAt(i) == '#') {
+		indexOfLastSlashOrFirstSharp = i;
+	    }
+	}
+	final String result = x.substring(indexOfLastSlashOrFirstSharp + 1);
+	// If it ends in a slash then remove the ending slash and try again.
+	if (result.length() == 0) {
+	    return substringAfterLastSlashOrFirstSharp(
+                       x.substring(0, x.length()-1));
+	} else {
+	    return result;
+	}
+    }
+}
+
+// End of file.
