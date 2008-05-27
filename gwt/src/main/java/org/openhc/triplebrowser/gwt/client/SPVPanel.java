74f4631ba489e8789cc71f4ac80834238e599093
Harold Carr <harold.carr@gmail.com>
2008-05-27 04:19:43 +0000

    ["Trying to get rid of Label in vertical panel.  But this does not\n", "horizontal scroll correctly and the scrolling gets stuck after a query.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/client/SPVPanel.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/client/SPVPanel.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Jun 14 (Wed) 18:29:38 by Harold Carr.
-// Last Modified : 2008 May 24 (Sat) 20:43:57 by Harold Carr.
+// Last Modified : 2008 May 26 (Mon) 21:10:14 by Harold Carr.
 //
 
 package org.openhc.trowser.gwt.client;
@@ -10,12 +10,14 @@ import java.util.Iterator;
 import java.util.List;
 
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ChangeListener;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Frame;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.ScrollPanel;
-import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 import org.openhc.trowser.gwt.client.Main;
 
@@ -25,10 +27,9 @@ public class SPVPanel
 
     private final String spvCategory;
     private List  contents;
-    private final VerticalPanel verticalInsideScroll;
+    private final ListBox listBoxInsideScroll;
     private final VerticalPanel topVerticalPanel;
     private final Button topButton;
-    private final ScrollPanel scrollPanel;
 
     public SPVPanel(final String spvCategory)
     {
@@ -42,12 +43,27 @@ public class SPVPanel
 	topButton = new Button(getPendingExpandCollapseState());
 	topVerticalPanel.add(topButton);
 	// TODO: Would like a scroll or a cloud
-	verticalInsideScroll = new VerticalPanel();
+	listBoxInsideScroll = new ListBox();
+	listBoxInsideScroll.setVisibleItemCount(10);
+	listBoxInsideScroll.addChangeListener(new ChangeListener() {
+	    public void onChange(Widget ignored) {
+		final String choice =
+		    ((SPVItem)contents.get(
+					   listBoxInsideScroll.getSelectedIndex()))
+		        .getExpandedName();
+		Main.getMainPanel().spvLinkClicked(
+		    spvCategory, choice);
+		Main.getMainPanel().getFrameCurrentSelection()
+		    .setUrl(choice);
+	    }
+	});
+	listBoxInsideScroll.setStyleName(Main.subjectPropertyValue);
+	listBoxInsideScroll.setVisibleItemCount(100);
 
-
-
-	scrollPanel = new ScrollPanel(verticalInsideScroll);
+	ScrollPanel scrollPanel = new ScrollPanel(listBoxInsideScroll);
 	scrollPanel.setStyleName(Main.subjectPropertyValue);
+	
+	//topVerticalPanel.add(listBoxInsideScroll);
 	topVerticalPanel.add(scrollPanel);
 	// End layout.
 	
@@ -81,13 +97,9 @@ public class SPVPanel
 	final List result = new ArrayList();
 	while (i.hasNext()) {
 	    String uri = (String) i.next();
-	    result.add(
-		       new SPVItem(spvCategory, 
+	    result.add(new SPVItem(spvCategory, 
 				   uri,
-				   substringAfterLastSlashOrFirstSharp(uri),
-				   // NOTE: during development change to
-				   // Main.expand to test full range.
-				   Main.collapse));
+				   substringAfterLastSlashOrFirstSharp(uri)));
 	}
 	return result;
     }
@@ -97,46 +109,34 @@ public class SPVPanel
     public void setContents(final List x)
     {
 	contents = convertContents(x);
-	verticalInsideScroll.clear();
+	listBoxInsideScroll.clear();
 	final Iterator i = contents.iterator();
 	while (i.hasNext()) {
 	    final SPVItem spvItem = (SPVItem) i.next();
-	    verticalInsideScroll.add(spvItem.getLabel());
-
-	    if (expandCollapseState.equals(Main.expand)) {
-		spvItem.getLabel().setText(spvItem.getExpandedName());
-	    }
-
-	    spvItem.getLabel().addClickListener(new ClickListener() {
-		public void onClick(final Widget sender) {
-		    // Causes doQuery so no history necessary here.
-		    Main.getMainPanel().spvLinkClicked(
-		        spvCategory, spvItem.getExpandedName());
-
-		    Main.getMainPanel().getFrameCurrentSelection()
-			.setUrl(spvItem.getExpandedName());
-		}
-	    });
+	    listBoxInsideScroll.addItem(
+                (expandCollapseState.equals(Main.expand) ?
+		 spvItem.getExpandedName() :
+		 spvItem.getCollapsedName()));
 	}
     }
 
     String expandOrCollapse()
     {
 	String pendingExpandCollapseState = getPendingExpandCollapseState();
-	final Iterator i = verticalInsideScroll.iterator();
+	//final Iterator i = listBoxInsideScroll.iterator();
 	final Iterator j = contents.iterator();
-	while (i.hasNext()) {
+	int n = 0;
+	while (j.hasNext()) {
 	    // TODO: redundant i.next - just need to make sure i and j
 	    // are same size.
-	    final Object object = i.next();
+	    //final Object object = i.next();
 	    final SPVItem spvItem = (SPVItem) j.next();
-	    if (spvItem.getCurrentExpandCollapseState().equals(Main.collapse)){
-		if (pendingExpandCollapseState.equals(Main.expand)) {
-		    spvItem.getLabel().setText(spvItem.getExpandedName());
-		} else {
-		    spvItem.getLabel().setText(spvItem.getCollapsedName());
-		}
+	    if (pendingExpandCollapseState.equals(Main.expand)) {
+		listBoxInsideScroll.setItemText(n, spvItem.getExpandedName());
+	    } else {
+		listBoxInsideScroll.setItemText(n, spvItem.getCollapsedName());
 	    }
+	    n++;
 	}
 	expandCollapseState = pendingExpandCollapseState;
 	return getPendingExpandCollapseState();
