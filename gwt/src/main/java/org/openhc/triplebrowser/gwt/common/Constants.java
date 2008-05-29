fde04f0ab90849125aca96636a323e973228515a
Harold Carr <harold.carr@gmail.com>
2008-05-29 22:24:29 +0000

    ["new constants.\n", ""]

--- gwt/src/main/java/org/openhc/triplebrowser/gwt/common/Constants.java
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/common/Constants.java
@@ -1,6 +1,6 @@
 //
 // Created       : 2006 Jun 14 (Wed) 18:29:38 by Harold Carr.
-// Last Modified : 2008 May 26 (Mon) 17:00:09 by Harold Carr.
+// Last Modified : 2008 May 29 (Thu) 15:16:21 by Harold Carr.
 //
 
 package org.openhc.trowser.gwt.common;
@@ -9,6 +9,7 @@ public interface Constants
 {
     // Only used by GWT version.
     public static final String serviceEntryPoint    = "/Service";
+    public static final String uploadFormHandler    = "/UploadFormHandler";
 
     public static final String clear                = "clear";
     public static final String asteriskSymbol       = "*";
@@ -16,16 +17,18 @@ public interface Constants
     public static final String collapse             = "-";
     public static final String copyright            = "copyright 2007";
     public static final String doQuery              = "doQuery";
-    public static final String emptyString          = "emptyString";
+    public static final String emptyString          = "";
     public static final String expand               = "+";
     public static final String expandOrCollapseSPVClick 
 	= "expandOrCollapseSPVClick";
     public static final String expandOrCollapseSPVItemClick
 	= "expandOrCollapseSPVItemClick";
     public static final String historyFieldSeparator = ":;:";
+    public static final String littleV              = "v";
     public static final String minusSymbol          = "-";
     public static final String plusSymbol           = "+";
     public static final String property             = "property";
+    public static final String querying             = "QUERYING ...";
     public static final String questionMarkSymbol   = "?";
     public static final String qsubject             = "?subject";
     public static final String qproperty            = "?property";
@@ -37,6 +40,7 @@ public interface Constants
     public static final String subject              = "subject";
     public static final String subjectPropertyValue = "subjectPropertyValue";
     public static final String trowserUploadedFile  = "trowserUploadedFile";
+    public static final String uploading            = "UPLOADING ...";
     public static final String url                  = "url";
     public static final String value                = "value";
 }
