2211751169d03ea76a4a66f20613c70880794138
Harold Carr <harold.carr@gmail.com>
2008-05-29 21:40:31 +0000

    ["Initial revision\n", ""]

---
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/common/Constants.java
@@ -0,0 +1,45 @@
+//
+// Created       : 2006 Jun 14 (Wed) 18:29:38 by Harold Carr.
+// Last Modified : 2008 May 26 (Mon) 17:00:09 by Harold Carr.
+//
+
+package org.openhc.trowser.gwt.common;
+
+public interface Constants
+{
+    // Only used by GWT version.
+    public static final String serviceEntryPoint    = "/Service";
+
+    public static final String clear                = "clear";
+    public static final String asteriskSymbol       = "*";
+    public static final String blankSpace           = " ";
+    public static final String collapse             = "-";
+    public static final String copyright            = "copyright 2007";
+    public static final String doQuery              = "doQuery";
+    public static final String emptyString          = "emptyString";
+    public static final String expand               = "+";
+    public static final String expandOrCollapseSPVClick 
+	= "expandOrCollapseSPVClick";
+    public static final String expandOrCollapseSPVItemClick
+	= "expandOrCollapseSPVItemClick";
+    public static final String historyFieldSeparator = ":;:";
+    public static final String minusSymbol          = "-";
+    public static final String plusSymbol           = "+";
+    public static final String property             = "property";
+    public static final String questionMarkSymbol   = "?";
+    public static final String qsubject             = "?subject";
+    public static final String qproperty            = "?property";
+    public static final String qvalue               = "?value";
+    public static final String shiftLeft            = "<-";
+    public static final String shiftRight           = "->";
+    public static final String showAll              = "show all";
+    public static final String showMatch            = "show match";
+    public static final String subject              = "subject";
+    public static final String subjectPropertyValue = "subjectPropertyValue";
+    public static final String trowserUploadedFile  = "trowserUploadedFile";
+    public static final String url                  = "url";
+    public static final String value                = "value";
+}
+
+// End of file.
+
