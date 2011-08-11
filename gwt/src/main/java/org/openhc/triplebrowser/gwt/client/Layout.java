4ef7de012a1204892b1de19f72ee1e4177b6829b
Harold Carr <harold.carr@gmail.com>
2011-08-11 03:48:15 +0000

    ["Initial revision\n", ""]

---
+++ gwt/src/main/java/org/openhc/triplebrowser/gwt/client/Layout.java
@@ -0,0 +1,23 @@
+//
+// Created       : 2006 Jun 14 (Wed) 18:29:38 by Harold Carr.
+// Last Modified : 2008 May 28 (Wed) 21:51:38 by Harold Carr.
+//
+
+package org.openhc.trowser.gwt.client;
+
+public class Layout
+{
+    public void queryPanelLayout()
+    {
+    }
+
+    public void spvPanelLayout()
+    {
+    }
+
+    public void mainPanelLayout()
+    {
+    }
+}
+
+// End of file.
