b59ef018bba5f0688b98becaaec4e4b7a9eba16c
Harold Carr <harold.carr@gmail.com>
2014-07-16 12:11:44 -0700

    Trying an unsafe trick from Jacco.

---
+++ haskell/src/Jacco.hs
@@ -0,0 +1,26 @@
+module Jacco where
+
+import           Graphics.UI.Threepenny as UI
+import           System.Random
+
+jacco :: IO ()
+jacco = startGUI defaultConfig setup
+
+setup :: Window -> UI ()
+setup window =
+    do btn  <- UI.button #+ [string "click"]
+       area <- UI.input
+
+       getBody window #+ [element btn, element area]
+
+       let e = unsafeMapIO (\_ -> sideEffects) (UI.click btn)
+       b <- stepper "" e
+
+       return area # sink value b
+
+       return ()
+
+sideEffects :: IO String
+sideEffects =
+    do x <- randomRIO (0,10) :: IO Int
+       return (show x)
