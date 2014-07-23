297c1090cfa0166d1b2afc161b26b17d1e7ad5af
Harold Carr <harold.carr@gmail.com>
2014-07-23 09:00:46 -0700

    Figuring out how Taldykin's example works.

--- haskell/src/Taldykin.hs
+++ haskell/src/Taldykin.hs
@@ -1,6 +1,6 @@
 {-
 Created       : 2014 Jul 17 (Thu) 04:21:11 by Max Taldykin.
-Last Modified : 2014 Jul 23 (Wed) 07:33:15 by Harold Carr.
+Last Modified : 2014 Jul 23 (Wed) 08:19:43 by Harold Carr.
 http://stackoverflow.com/questions/24784883/using-threepenny-gui-reactive-in-client-server-programming
 -}
 
@@ -8,33 +8,54 @@ module Taldykin where
 
 import           Graphics.UI.Threepenny as UI
 
+{-
+Event represents a stream of events in time.
+Like an infinite list of values tagged with their time of occurence: `type Event a = [(Time,a)]`
+
+`Behavior a` represents time varying value. Like: `type Behavior a = Time -> a`
+
+Event handler is function given event value.  Performs some computation. `type Handler a = a -> IO ()Source`
+-}
+
 main :: IO ()
 main = do
-    (evFillList, doFillList) <- newEvent
+    -- newEvent :: IO (Event a, Handler a)
+    (eventFillListBox, handlerFillListBox) <- newEvent
+    -- valuesSupply :: String -> IO [String]
     initialList <- valuesSupply ""
-    behFillList <- stepper initialList evFillList
+    -- stepper :: MonadIO m => a -> Event a -> m (Behavior a)
+    -- Construct time-varying function from initial value and stream of new values.
+    behaviorFillListBox <- stepper initialList eventFillListBox
 
     startGUI defaultConfig $ \win -> do
         list <- ul
-        sel <- listBox behFillList
-                       (pure Nothing)
-                       (pure $ \it -> UI.span # set text it)
+        -- listBox :: Ord a => Behavior [a]               -- list of items
+        --                  -> Behavior (Maybe a)         -- selected item
+        --                  -> Behavior (a -> UI Element) -- display for an item
+        --                  -> UI (ListBox a)             -- created ListBox
+        lstBox <- listBox behaviorFillListBox
+                          (pure Nothing)
+                          (pure $ \it -> UI.span # set text it)
+        element lstBox # set (attr "size") "10" # set style [("width","200px")]
 
-        getBody win #+ [grid [[element list, element sel]]]
-        setFocus $ getElement sel
+        getBody win #+ [element lstBox, element list]
+        setFocus $ getElement lstBox
 
-        on selectionChange (getElement sel) $ \x -> case x of
+        -- on :: (element -> Event a) -> element -> (a -> UI void) -> UI ()
+        -- selectionChange :: Element -> Event (Maybe Int)
+        on selectionChange (getElement lstBox) $ \x -> case x of
             Nothing -> return ()
-            Just ix -> do items <- currentValue behFillList
+            Just ix -> do -- currentValue :: MonadIO m => Behavior a -> m a
+                          items <- currentValue behaviorFillListBox
                           let it = items !! ix
-                          liftIO $ valuesSupply it >>= doFillList
+                          liftIO $ valuesSupply it >>= handlerFillListBox
                           element list #+ [li # set html it]
-                          setFocus $ getElement sel
+                          setFocus $ getElement lstBox
 
 
 valuesSupply :: String -> IO [String]
 valuesSupply x = do
-    putStrLn "YES"
+    putStrLn $ "YES: " ++ x
     return [x ++ show i | i <- [0..9::Int]]
 {- This works
 valuesSupply x = do
