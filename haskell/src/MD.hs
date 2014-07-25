69c58fa5c7e59b3ecd0c0f70ca6757f6bb55a61c
Harold Carr <harold.carr@gmail.com>
2014-07-25 08:36:45 -0700

    Wiring complete.  Ready to do SPARQL queries.

--- haskell/src/MD.hs
+++ haskell/src/MD.hs
@@ -1,6 +1,6 @@
 {-
 Created       : by threepenny-gui MissingDollars sample.
-Last Modified : 2014 Jul 25 (Fri) 08:20:12 by Harold Carr.
+Last Modified : 2014 Jul 25 (Fri) 08:35:15 by Harold Carr.
 -}
 
 module MD where
@@ -13,7 +13,7 @@ data SPVType = SUB | PRE | VAL
 
 main :: IO ()
 main = do
-    (subFRP : preFRP : valFRP : _) <- replicateM 3 $ mkListBoxFRP valuesSupply
+    (subFRP : preFRP : valFRP : _) <- replicateM 3 $ mkListBoxFRP
     startGUI defaultConfig $ \w -> do
         return w # set title "MD"
         getBody w #+ [row [ mkLayout subFRP preFRP valFRP ] ]
@@ -79,13 +79,13 @@ mkLayout (hSubFillListBox, bSubFillListBox)
         doQuery
     }
 
-    subListBox <- mkListBox SUB hSubFillListBox bSubFillListBox doSelectionQuery
-    preListBox <- mkListBox PRE hPreFillListBox bPreFillListBox doSelectionQuery
-    valListBox <- mkListBox VAL hValFillListBox bValFillListBox doSelectionQuery
+    subListBox <- mkListBox SUB bSubFillListBox doSelectionQuery
+    preListBox <- mkListBox PRE bPreFillListBox doSelectionQuery
+    valListBox <- mkListBox VAL bValFillListBox doSelectionQuery
 
-    frm <- UI.frameset #+ [ UI.frame # set (attr "name") "top"
+    frm <- UI.frameset #+ [ UI.frame # set (attr "name")   "top"
                                      # set (attr "target") "top"
-                                     # set (attr "src") "http://haroldcarr.com/" ]
+                                     # set (attr "src")    "http://haroldcarr.com/" ]
 
     grid [ [ row [ element sparqlEndpointURL, element submit ] ]
          , [ row [ column [ row [ element ddSub, element currentSub ]
@@ -105,19 +105,17 @@ mkLayout (hSubFillListBox, bSubFillListBox)
          , [ element frm ]
          ]
 
-mkListBoxFRP :: ([Char] -> IO a) -> IO (Handler a, Behavior a)
-mkListBoxFRP valuesSupply0 = do
+mkListBoxFRP :: IO (Handler [String], Behavior [String])
+mkListBoxFRP = do
     (eventFillListBox, handlerFillListBox) <- newEvent
-    initialList         <- valuesSupply0 ""
-    behaviorFillListBox <- stepper initialList eventFillListBox
+    behaviorFillListBox <- stepper [] eventFillListBox
     return (handlerFillListBox, behaviorFillListBox)
 
 mkListBox :: SPVType
-             -> ([String] -> IO a)
              -> Behavior [String]
              -> ((SPVType,String) -> UI ())
              -> UI (UI.ListBox String)
-mkListBox spvType hFillListBox bFillListBox sel = do
+mkListBox spvType bFillListBox sel = do
     listBox <- UI.listBox bFillListBox
                           (pure Nothing)
                           (pure $ \it -> UI.span # set text it)
@@ -128,7 +126,6 @@ mkListBox spvType hFillListBox bFillListBox sel = do
         Just ix -> do items <- currentValue bFillListBox
                       let it = items !! ix
                       sel (spvType, it)
---                      liftIO $ valuesSupply it >>= hFillListBox
                       UI.setFocus $ getElement listBox
     return listBox
 
@@ -136,14 +133,10 @@ mkListBox spvType hFillListBox bFillListBox sel = do
 
 doRDFQuery :: String -> String -> String -> String -> IO ([String],[String],[String])
 doRDFQuery endpoint s p v = do
+    putStrLn endpoint
     let s' = [ s ++ (show i) | i <- [0..19::Int]]
     let p' = [ p ++ (show i) | i <- [0..19::Int]]
     let v' = [ v ++ (show i) | i <- [0..19::Int]]
     return (s',p',v')
-
-valuesSupply :: String -> IO [String]
-valuesSupply x = do
-    putStrLn $ "YES: " ++ x
-    return [x ++ show i | i <- [0..19::Int]]
 
 -- End of file.
