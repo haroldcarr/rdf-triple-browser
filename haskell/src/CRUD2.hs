88ca45c4f776762d99c7853170312daf2e262441
Harold Carr <harold.carr@gmail.com>
2014-08-12 20:10:03 -0700

    Cleanup and renames.

--- haskell/src/CRUD2.hs
+++ haskell/src/CRUD2.hs
@@ -1,6 +1,6 @@
 {-
 Created       : by threepenny-gui/samples/CRUD
-Last Modified : 2014 Aug 12 (Tue) 19:48:28 by Harold Carr.
+Last Modified : 2014 Aug 12 (Tue) 20:06:08 by Harold Carr.
 -}
 
 {-# LANGUAGE RecursiveDo #-}
@@ -14,7 +14,6 @@ import qualified Data.Text                   as T (pack)
 import           Database.HSparql.Connection
 import qualified Graphics.UI.Threepenny      as UI
 import           Graphics.UI.Threepenny.Core
-import           Prelude                     hiding (lookup)
 import           RTBQ
 
 ------------------------------------------------------------------------------
@@ -26,19 +25,16 @@ main =
         getBody window #+ [ mkSPVPanel "?subject" ]
         return ()
 
-mkSPVPanel :: String
-              -> UI Element
+mkSPVPanel :: String -> UI Element
 mkSPVPanel spvType = mdo
-    let decide db = if dbSize db > 1
-                       then "?"
-                       else case lookup 0 db of
-                                Just (s,_) -> s
-                                Nothing    -> error "NOT SUPPOSED TO HAPPEN"
+    let bad = ("NOT SUPPOSED TO HAPPEN", Bound (UNode (T.pack "BAD")))
+        decide db | dbSize db > 1 = spvType
+                  | otherwise     = fst $ fromMaybe bad (dbLookup 0 db)
 
         dataItem :: Behavior (Maybe DI) -> UI Element
         dataItem bItem = do
             entry1 <- UI.entry $ decide <$> bDB
-            element entry1 # set style [("width", "400px")]
+            element entry1 # set style [("width", "800px")]
             return $ getElement  entry1
 
     -- GUI elements
@@ -46,7 +42,7 @@ mkSPVPanel spvType = mdo
     addToLBBtn  <- UI.button #+ [string "Add To List Box"]
     listBox     <- UI.listBox  bLBItems bLBSelection bDisplayDI
     lbSelection <- dataItem    bLBSelectionDI
-    let uiDI = grid [[string spvType, element lbSelection]]
+    let uiDI = grid [[element lbSelection]]
     element listBox # set (attr "size") "20" # set style [("width","800px")]
 
     -- events and behaviors
@@ -68,13 +64,13 @@ mkSPVPanel spvType = mdo
 
     -- database
     let dbFill                    :: [(String,BindingValue)] -> DB DI -> DB DI
-        dbFill    ss _            = foldr create  emptydb ss
+        dbFill    ss _            = foldr dbCreate  dbEmpty ss
 
         dbAddToLB                 :: DB DI -> DB DI
         dbAddToLB (DB newkey db0) = DB newkey $ Map.map (\(x,y) -> (x ++ "xx", y)) db0
 
     -- bDB :: Behavior (DB DI)
-    bDB <- accumB emptydb $ concatenate <$> unions
+    bDB <- accumB dbEmpty $ concatenate <$> unions
         [ dbFill    <$> eFillLB
         , dbAddToLB <$  eAddToLB
         ]
@@ -84,13 +80,13 @@ mkSPVPanel spvType = mdo
     bLBSelection <- stepper Nothing eLBSelection
 
     let bLookup :: Behavior (DBKey -> Maybe DI)
-        bLookup = flip lookup <$> bDB
+        bLookup = flip dbLookup <$> bDB
 
         bDisplayDI :: Behavior (DBKey -> UI Element)
         bDisplayDI = (UI.string .) <$> (maybe "" showDI .) <$> bLookup
 
         bLBItems :: Behavior [DBKey]
-        bLBItems = keys <$> bDB
+        bLBItems = dbKeys <$> bDB
 
         bLBSelectionDI :: Behavior (Maybe DI)
         bLBSelectionDI = (=<<) <$> bLookup <*> bLBSelection
@@ -109,20 +105,20 @@ mkSPVPanel spvType = mdo
 type DBKey = Int
 data DB a  = DB { nextKey :: !Int, db :: Map.Map DBKey a }
 
-emptydb :: DB a
-emptydb = DB 0 Map.empty
+dbEmpty  :: DB a
+dbEmpty  = DB 0 Map.empty
 
-dbSize :: DB a -> Int
-dbSize = Map.size . db
+dbSize   :: DB a -> Int
+dbSize   = Map.size . db
 
-keys :: DB a -> [DBKey]
-keys    = Map.keys . db
+dbKeys   :: DB a -> [DBKey]
+dbKeys   = Map.keys . db
 
-create :: a -> DB a -> DB a
-create x     (DB newkey db0) = DB (newkey+1) $ Map.insert newkey x db0
+dbCreate :: a -> DB a -> DB a
+dbCreate x     (DB newkey db0) = DB (newkey+1) $ Map.insert newkey x db0
 
-lookup :: DBKey -> DB a -> Maybe a
-lookup key   (DB _      db0) = Map.lookup                       key      db0
+dbLookup :: DBKey -> DB a -> Maybe a
+dbLookup key   (DB _      db0) = Map.lookup                       key      db0
 
 ------------------------------------------------------------------------------
 -- What is stored in data base
@@ -145,7 +141,7 @@ doRDFQuery mk bdb = do
     putStrLn ("MK : " ++ show mk)
     db0 <- currentValue bdb
     case mk of
-        (Just k) -> do let v = lookup k db0
+        (Just k) -> do let v = dbLookup k db0
                        putStrLn ("DBValue: " ++ show v)
                        case v of
                            Just b -> do rrr <- ttwv b
