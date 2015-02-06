18dd58c31b84be77eb0f82ff6cba327c4514fa7c
Harold Carr <harold.carr@gmail.com>
2015-02-05 18:04:20 -0800

    Cleanup threepenny external newEvent demo.

--- experiments/haskell-threepenny-gui-experiments/src/A.hs
+++ experiments/haskell-threepenny-gui-experiments/src/A.hs
@@ -1,41 +1,40 @@
 {-
 Created       : 2015 Feb 05 (Thu) 14:56:08 by Harold Carr.
-Last Modified : 2015 Feb 05 (Thu) 17:22:53 by Harold Carr.
+Last Modified : 2015 Feb 05 (Thu) 18:01:53 by Harold Carr.
 -}
 
 module A where
 
 import           Control.Concurrent     (forkIO)
-import           Graphics.UI.Threepenny as UI
+import           Graphics.UI.Threepenny
 import           Network
+import           System.IO              (hClose)
 
 main :: IO ()
 main = do
-    (ev, hnd) <- newEvent
-    bFillLB <- stepper "" ev
-    forkIO (a1 hnd 8888)
-    forkIO (a1 hnd 9999)
+    (eAccept, hAccept) <- newEvent
+    bAccept <- stepper "" eAccept
+    forkIO (listenAndAccept hAccept 6789)
+    forkIO (listenAndAccept hAccept 9876)
     startGUI defaultConfig $ \win -> do
-        lstBox <- listBox bFillLB
-                          (pure Nothing)
-                          (pure $ \it -> UI.span # set text [it])
-        element lstBox # set (attr "size") "10" # set style [("width","200px")]
-        getBody win #+ [element lstBox]
+        entree <- entry bAccept
+        element entree # set (attr "size") "10" # set style [("width","200px")]
+        getBody win #+ [element entree]
         return ()
 
-a1 :: (String -> IO a) -> PortNumber -> IO ()
-a1 hnd bindAddr = do
-    s <- listenOn  $ PortNumber bindAddr
-    acceptLoop hnd bindAddr s
+listenAndAccept :: (String -> IO a) -> PortNumber -> IO ()
+listenAndAccept hAccept bindAddr = do
+    s <- listenOn $ PortNumber bindAddr
+    acceptLoop hAccept bindAddr s
     return ()
 
 acceptLoop :: (String -> IO a) -> PortNumber -> Socket -> IO b
-acceptLoop hnd bindAddr s = do
-    putStrLn "about to accept"
-    (_, hostname, portNumber) <- accept s
-    putStrLn hostname
-    putStrLn $ show portNumber
-    hnd $ show bindAddr
-    acceptLoop hnd bindAddr s
+acceptLoop hAccept bindAddr s = loop
+  where
+    loop = do
+        (h, hostname, portNumber) <- accept s
+        hClose h
+        hAccept $ show bindAddr ++ " " ++ hostname ++ " " ++ show portNumber
+        loop
 
 -- End of file.
